cb698ab7dda0b4befd58b6898263544cafb2af95
Luis <luishenriquecorporativo@gmail.com>
2023-08-08 16:39:16 -0300

    Primeiro commit

---
+++ src/main/java/br/gov/sp/fatec/hello/controller/StudentController.java
@@ -0,0 +1,40 @@
+package br.gov.sp.fatec.hello.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.gov.sp.fatec.hello.entities.Student;
+
+@RestController
+@RequestMapping("students")
+public class StudentController {
+
+
+    @GetMapping
+    public List<Student> getStudent(){
+
+
+        List<Student> list = new ArrayList<Student>();
+
+        Student s1 = new Student();
+        s1.setCourse("ADS");
+        s1.setId(1);
+        s1.setName("Glauco Todesco");
+
+        Student s2 = new Student();
+        s2.setCourse("ADS");
+        s2.setId(2);
+        s2.setName("Glauco Todesco");
+
+        list.add(s1);
+        list.add(s2);
+
+        return list;
+        
+    }
+    
+}
